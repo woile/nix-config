e32a32b99105e42a55c10e10de07716d80637a5f
Santiago Fraire Willemoes <santiwilly@gmail.com>
2025-04-04 09:04:08 -0300

    feat: update

--- flake.nix
+++ flake.nix
@@ -67,13 +67,4 @@
         specialArgs = { inherit inputs; };
       };
     };
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
 }
