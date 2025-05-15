c985ba751c1c15b9349c67531d173508aeb3dcbc
Santiago Fraire Willemoes <santiwilly@gmail.com>
2025-05-15 14:39:32 +0100

    feat: add usbutils

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -162,6 +162,7 @@
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
+    usbutils
 
     # kde
     kdePackages.kcalc
