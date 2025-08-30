de2e38f70d2f7615b2736b069a94873a25320467
Santiago Fraire Willemoes <santiwilly@gmail.com>
2025-08-30 08:24:00 +0100

    feat: add docker to nixos host

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -287,4 +287,22 @@
       allowUnfreePredicate = _: true;
     };
   };
+
+  virtualisation.docker = {
+    # Consider disabling the system wide Docker daemon
+    enable = false;
+
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+      # Optionally customize rootless Docker daemon settings
+      daemon.settings = {
+        dns = [
+          "1.1.1.1"
+          "8.8.8.8"
+        ];
+      };
+    };
+  };
+
 }
