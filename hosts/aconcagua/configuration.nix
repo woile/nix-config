0293d8c385de852ed8884a7ef48a58c037ec5bfa
Santiago Fraire Willemoes <santiwilly@gmail.com>
2025-10-24 15:34:30 +0100

    fix: fmt new files

--- hosts/aconcagua/configuration.nix
+++ hosts/aconcagua/configuration.nix
@@ -2,13 +2,18 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ../../hardware/aistone/x4sp4nal/hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ../../hardware/aistone/x4sp4nal/hardware-configuration.nix
+  ];
+
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth.settings.General.Experimental = true;
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -16,8 +21,6 @@
 
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  boot.initrd.luks.devices."luks-6606e271-2d02-4664-a7b5-4d0783dc2cc1".device = "/dev/disk/by-uuid/6606e271-2d02-4664-a7b5-4d0783dc2cc1";
   networking.hostName = "aconcagua"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
@@ -29,7 +32,7 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "Europe/Lisbon";
+  # time.timeZone = "Europe/Lisbon";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -45,6 +48,87 @@
     LC_TELEPHONE = "pt_PT.UTF-8";
     LC_TIME = "pt_PT.UTF-8";
   };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.woile = {
+    isNormalUser = true;
+    description = "Santiago Fraire";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "kvm"
+      "adbusers"
+      "libvirtd"
+    ];
+    packages = with pkgs; [
+      vim
+      rng-tools
+      #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+  programs.partition-manager.enable = true;
+  programs.nix-ld = {
+    enable = true;
+  };
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
+  };
+
+  # Virtualisation
+  programs.virt-manager.enable = true;
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+  services.spice-vdagentd.enable = true; # enable copy and paste between host and guest
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
+    usbutils
+
+    # kde
+    kdePackages.kcalc
+    catppuccin-kde
+    kdePackages.kate # text-editor
+    kdePackages.kgpg
+    kdePackages.merkuro # calendar
+    kdePackages.francis # pomodoro
+    kdePackages.koi # dark/light auto-switch
+    kdePackages.okular # PDF viewer
+    kdePackages.kontrast # color contrast
+    kdePackages.calligra # office suite
+    kdePackages.akonadi # contacts
+    # clipboard
+    wl-clipboard-rs
+
+    # used to sign PDF by okular
+    nss_latest
+
+    # For steam games
+    protonup-qt
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # automatic timezone
+  services.automatic-timezoned.enable = true;
+  services.geoclue2.geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
@@ -62,6 +146,15 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.cups-pdf.enable = false;
+  services.printing.cups-pdf.instances = {
+    pdf = {
+      settings = {
+        Out = "\${HOME}/pdf-printed";
+        UserUMask = "0033";
+      };
+    };
+  };
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
@@ -79,48 +172,23 @@
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.woile = {
-    isNormalUser = true;
-    description = "Santiago Fraire";
-    extraGroups = [ "networkmanager" "wheel" "kvm" "adbusers" "libvirtd"];
-    packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
+  services.ollama = {
+    enable = true;
+    acceleration = "rocm";
+    loadModels = [
+      "hf.co/bartowski/zed-industries_zeta-GGUF:Q5_K_M"
+      "qwen3:8b"
+      "gemma3n:e4b"
+      "hf.co/unsloth/gemma-3n-E4B-it-GGUF:Q4_K_XL"
+      "hf.co/unsloth/Qwen3-8B-GGUF:Q4_K_XL"
+      "hf.co/unsloth/Qwen3-14B-GGUF:Q4_K_XL"
+      "hf.co/unsloth/medgemma-4b-it-GGUF:Q4_K_XL"
     ];
   };
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-    wget
-    rng-tools
-  #  wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.flatpak.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -135,5 +203,47 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
+  nix = {
+    # Store optimization
+    optimise = {
+      automatic = true;
+      dates = [ "13:00" ];
+    };
 
+    # Garbage collection
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+
+    settings = {
+      trusted-users = [ "@wheel" ];
+
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+        "https://reciperium.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "reciperium.cachix.org-1:xAmT5McauMNqMlXkkyVzDzoDNO6G+Zo7gCAUYaPsGxQ="
+      ];
+    };
+  };
+
+  # nixpkgs instance config
+  nixpkgs = {
+    config = {
+      # Always allow unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
 }
