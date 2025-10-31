6f7f44f6e2078f617f9240104ff0c66817c3640b
Santiago Fraire Willemoes <santiwilly@gmail.com>
2025-10-31 07:36:58 +0000

    fix: allow cudasupport to nixos host

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -100,6 +100,8 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  nixpkgs.config.cudaSupport = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
