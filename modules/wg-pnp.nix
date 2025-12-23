ce399054ffbd7733e2ef3cf77d9bd87d68c729c4
Santiago Fraire Willemoes <santiwilly@gmail.com>
2025-12-23 19:46:47 +0000

    fix: minor change

--- modules/wg-pnp.nix
+++ modules/wg-pnp.nix
@@ -86,7 +86,7 @@ with lib;
             echo "Mapped new $protocol port $public_port, old one was $old_port."
             echo "$public_port" >"$port_file"
 
-            # --- INPUT Rule (Open the Public Port) ---
+            # --- INPUT Rule (Open the Public Port) --- d
             if ${pkgs.iptables}/bin/iptables -C INPUT -p "$protocol" --dport "$public_port" -j ACCEPT -i $GATEWAY
             then
               echo "New $protocol port $public_port already open, not opening again."
