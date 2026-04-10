d619a03f1df1e6a198a807715351674525e548cf
Santiago Fraire Willemoes <santiwilly@gmail.com>
2026-04-10 09:14:23 +0100

    feat: add woile as trusted substituter

--- profiles/laptop/configuration.nix
+++ profiles/laptop/configuration.nix
@@ -133,11 +133,13 @@
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
         "https://reciperium.cachix.org"
+        "https://woile.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "reciperium.cachix.org-1:xAmT5McauMNqMlXkkyVzDzoDNO6G+Zo7gCAUYaPsGxQ="
+        "woile.cachix.org-1:i67QD9uyZmig2S6qZ+r+ADyboWjbTyWn3DBmBYKJk+E="
       ];
     };
   };
