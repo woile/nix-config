0d1ccb6d68ef5bbcbd6b5fd1d067c08c2b45596e
Santiago Fraire Willemoes <santiwilly@gmail.com>
2026-05-09 09:26:00 +0100

    feat(amaru): working reverse proxy

--- hosts/amaru/configuration.nix
+++ hosts/amaru/configuration.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   modulesPath,
+  config,
   ...
 }:
 
@@ -20,13 +21,22 @@
   };
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   system.stateVersion = "26.01";
-  networking.hostName = "amaru";
 
+  networking.hostName = "amaru";
   networking.enableIPv6 = true;
   networking.useDHCP = true;
   networking.useNetworkd = true;
-  systemd.network.enable = true;
 
+  networking.interfaces.ens2.ipv6.addresses = [
+    {
+      # Block assigned by Scaleway when created a Flexible IPv6
+      # We make NixOS listen to all of it
+      address = "2001:bc8:1d90:1f4f::";
+      prefixLength = 64;
+    }
+  ];
+
+  systemd.network.enable = true;
   nix.settings = {
     trusted-users = [
       "root"
@@ -73,5 +83,68 @@
     enable = true;
     settings.PasswordAuthentication = false;
     settings.PermitRootLogin = "prohibit-password";
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+  services.traefik = {
+    enable = true;
+    staticConfigOptions = {
+      entryPoints = {
+        web = {
+          address = "[::]:80";
+          asDefault = true;
+          http.redirections.entrypoint = {
+            to = "websecure";
+            scheme = "https";
+          };
+        };
+
+        websecure = {
+          address = "[::]:443";
+          asDefault = true;
+          http.tls.certResolver = "letsencrypt";
+        };
+      };
+
+      log = {
+        level = "INFO";
+        format = "json";
+      };
+
+      certificatesResolvers.letsencrypt.acme = {
+        email = "santiwilly@gmail.com";
+        storage = "${config.services.traefik.dataDir}/acme.json";
+        httpChallenge.entryPoint = "web";
+      };
+
+      ping = {
+        manualRouting = true;
+      };
+      # Access the Traefik dashboard on <Traefik IP>:8080 of your server
+      # api.dashboard = true;
+      # api.insecure = true;
+    };
+    # Dynamic Configuration
+    dynamicConfigOptions = {
+      http = {
+        routers = {
+          auth-router = {
+            rule = "Host(`auth.woile.dev`)";
+            entryPoints = [ "websecure" ];
+            # Route traffic directly to Traefik's internal ping service
+            service = "ping@internal";
+            tls = {
+              certResolver = "letsencrypt";
+            };
+          };
+        };
+
+        # Notice we deleted the 'services' block entirely!
+        # Because ping@internal is built-in, we don't need to define it.
+      };
+    };
   };
 }
