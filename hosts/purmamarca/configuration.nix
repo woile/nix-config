e30da00d4917b23faf7d179b244dc585eb88348a
Santiago Fraire Willemoes <santiwilly@gmail.com>
2026-05-22 10:07:15 +0100

    fix(kernel): swap to 7.0.6 until bluetooth is fixed

--- hosts/purmamarca/configuration.nix
+++ hosts/purmamarca/configuration.nix
@@ -29,6 +29,19 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackagesFor (
+    pkgs.linux_latest.override {
+      argsOverride = rec {
+        version = "7.0.6";
+        modDirVersion = "7.0.6";
+        src = pkgs.fetchurl {
+          url = "mirror://kernel/linux/kernel/v7.x/linux-${version}.tar.xz";
+          sha256 = "08vm18wx6399phzgr3wz94yga3ab4fyca79445ygvbspm904996b";
+        };
+      };
+    }
+  );
 
   networking.hostName = "purmamarca";
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
