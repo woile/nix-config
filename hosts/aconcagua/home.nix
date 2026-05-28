fd4f41b910326bd85179e1e161701ca92c152c8c
Santiago Fraire Willemoes <santiwilly@gmail.com>
2026-05-28 08:45:01 +0100

    fix(aconcagua): clean home

--- hosts/aconcagua/home.nix
+++ hosts/aconcagua/home.nix
@@ -10,16 +10,5 @@
     finamp
     jellyfin-desktop
     mistral-rs
-
-    (llama-cpp-rocm.overrideAttrs (oldAttrs: {
-      DGFX_VERSION = "11.5.1";
-      # FORCE_REBUILD = builtins.currentTime;
-      # Adding a dummy attribute forces a local build
-      passthru = (oldAttrs.passthru or { }) // {
-        forceRebuild = 1;
-      };
-    }))
-
   ];
-  # home.shellAliases = {};
 }
